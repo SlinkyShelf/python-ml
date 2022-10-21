8c22e9a07984cd00962e6b0df99e600532e0cad6
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-10-20 23:04:27 -0500

    Removed unneeded comments

--- hand-written-digits/index.py
+++ hand-written-digits/index.py
@@ -7,8 +7,6 @@ def toInt(_byte):
 labelsf = open(labelsPath, "rb")
 magicNumber= toInt(labelsf.read(4))
 labelCount = toInt(labelsf.read(4))
-# for i in range(100):
-#     print(toInt(labelsf.read(1)))
 
 imgsf = open(imgPath, "rb")
 magicNumber2 = toInt(imgsf.read(4))
@@ -16,12 +14,3 @@ imgCount = toInt(imgsf.read(4))
 imgRows = toInt(imgsf.read(4))
 imgCollums = toInt(imgsf.read(4))
 
-
-# for i in range(imgRows):
-#     row = ""
-#     for j in range(imgCollums):
-#         if toInt(imgsf.read(1)) < 128:
-#             row += "█"
-#         else:  
-#             row += " "
-#     print(row)
